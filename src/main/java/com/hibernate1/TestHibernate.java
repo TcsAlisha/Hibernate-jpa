6ace086b28e727768fa107de7d085a81d87d0cbc
TcsAlisha <tcsalishadias@gmail.com>
2021-08-24 04:27:17 +0000

    jpa hibernate

---
+++ src/main/java/com/hibernate1/TestHibernate.java
@@ -0,0 +1,24 @@
+package com.hibernate1;
+
+import org.hibernate.Session;
+
+
+public class TestHibernate {
+	public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        
+        //Add new Employee object
+        EmployeeEntity emp = new EmployeeEntity();
+        emp.setEmail("Lyrica.d@mail.com");
+        emp.setFirstName("Lyrica");
+        emp.setLastName("Dodati");
+         
+         
+        session.save(emp);
+         
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+	}
+}
+
